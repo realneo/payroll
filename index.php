9a98a2ff914a6bb4ba2be0f1351b35e98be05944
realneo <neo@yoteyote.com>
2014-09-29 09:26:42 +0300

    Creates a template of the Layout

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
     include_once('lib/config/autoload.php');
-    
-    
-    
 ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Payroll</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    </head>
+    <body>
+        <div id='top_header'>
+            
+        </div><!-- top_header -->
+        
+        <div id='container'>
+            
+        </div><!-- container -->
+        
+        <div id='footer'>
+            
+        </div><!-- footer -->
+        
+    </body>
+</html>    
