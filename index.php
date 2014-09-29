b8dbbf52c31d0ede27d0b217a7db8bbaa85ac5ec
realneo <neo@yoteyote.com>
2014-09-29 11:46:11 +0300

    Adds the Icons to the index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-    include_once('lib/config/autoload.php');
-?>
+<?php include_once('lib/config/autoload.php'); ?>
 
 <!DOCTYPE html>
 
@@ -8,22 +6,63 @@
     <head>
         <title>Payroll</title>
         <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome/css/font-awesome.min.css">
     </head>
     <body>
         <div id='top_header'>
             <div class='container'>
                 <div id='top_logo'></div><!-- top_logo -->
-                <div id='top_company_name'><h3>Home Shopping Center</h3></div>
-                <div id='software_name'>Payroll : Version 1.0</div>
+                <div class='text-lighter text-20 pull-left padding-10'><h3>Home Shopping Center</h3></div>
+                <div id='software_name' class='text-lighter'>Payroll : Version 1.0</div>
             </div><!-- .container -->
         </div><!-- top_header -->
         
         <div id='container'>
-            
+            <div class='container'>
+               <div id='home_icons'>
+                    <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-users fa-4x padding-left-10 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Employees</p>
+                    </div>
+
+                    <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-money fa-4x padding-left-10 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Salaries</p>
+                    </div>
+
+                    <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-pie-chart fa-4x padding-left-15 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Salary Advance</p>
+                    </div>
+
+                    <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-bullseye fa-4x padding-left-20 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Loans</p>
+                    </div>
+
+                   <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-gift fa-4x padding-left-15 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Bonus</p>
+                    </div>
+
+                    <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-file-o fa-4x padding-left-20 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Reports</p>
+                    </div>
+
+                    <div class='well large-icon pull-left margin-10'>
+                        <i class="fa fa-cog fa-4x padding-left-15 text-desaturated-blue"></i>
+                        <p class='text-center text-desaturated-blue text-lighter text-12'>Settings</p>
+                    </div>
+                </div><!-- home_icons -->
+                
+            </div>
         </div><!-- container -->
         
         <div id='footer'>
-            
+            <div class='container'>
+                <div class='pull-right text-11 text-lighter'>Software Developed by Yoteyote 2014</div>
+            </div><!-- container -->
         </div><!-- footer -->
         
     </body>
